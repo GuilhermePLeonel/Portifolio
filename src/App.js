c213626098694dea916c23034c0ff0a36238a800
GuilhermePLeonel <guilhermepleonel1997@gmail.com>
2023-02-20 14:10:34 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+    <Routes>
+      <Route path="/" element={ <Navigate to="/home" /> } />
+      <Route path="/home" element={ <Home /> } />
+    </Routes>
+    </div>
+  );
+}
+export default App;
